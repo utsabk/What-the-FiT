2c54a6c506a27fa99597896cd4fbe90717f6f26a
utsabk <kharelutsab@gmail.com>
2019-10-07 20:56:27 +0000

    Profile page with achievements and records

--- app/src/main/java/com/example/runningapp/CollectDataActivity.kt
+++ app/src/main/java/com/example/runningapp/CollectDataActivity.kt
@@ -14,31 +14,57 @@ class CollectDataActivity : AppCompatActivity() {
         val sharedPreferences =
             getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE)
 
-        profile_first_name.setText(sharedPreferences.getString(getString(R.string.preference_first_name), ""))
-        profile_last_name.setText(sharedPreferences.getString(getString(R.string.preference_last_name), ""))
-        profile_weight.setText(sharedPreferences.getInt(getString(R.string.preference_weight), 70).toString())
-        profile_height.setText(sharedPreferences.getInt(getString(R.string.preference_height), 165).toString())
-        profile_gender.setSelection(sharedPreferences.getInt(getString(R.string.preference_gender), 0))
+        data_collect_first_name.setText(
+            sharedPreferences.getString(
+                getString(R.string.preference_first_name),
+                ""
+            )
+        )
+        data_collect_last_name.setText(
+            sharedPreferences.getString(
+                getString(R.string.preference_last_name),
+                ""
+            )
+        )
+        data_collect_weight.setText(
+            sharedPreferences.getInt(
+                getString(R.string.preference_weight),
+                70
+            ).toString()
+        )
+        data_collect_height.setText(
+            sharedPreferences.getInt(
+                getString(R.string.preference_height),
+                165
+            ).toString()
+        )
+        data_collect_gender.setSelection(
+            sharedPreferences.getInt(
+                getString(R.string.preference_gender),
+                0
+            )
+        )
 
-        profile_save.setOnClickListener {
+        data_collect_save.setOnClickListener {
             try {
-                val firstName = profile_first_name.text.toString()
-                val lastName = profile_last_name.text.toString()
-                val weight = profile_weight.text.toString().toInt()
-                val height = profile_height.text.toString().toInt()
-                val gender = profile_gender.selectedItemPosition
+                val firstName = data_collect_first_name.text.toString()
+                val lastName = data_collect_last_name.text.toString()
+                val weight = data_collect_weight.text.toString().toInt()
+                val height = data_collect_height.text.toString().toInt()
+                val gender = data_collect_gender.selectedItemPosition
                 with(sharedPreferences.edit()) {
-                    putString(getString(R.string.preference_first_name),firstName )
-                    putString(getString(R.string.preference_last_name),lastName)
+                    putString(getString(R.string.preference_first_name), firstName)
+                    putString(getString(R.string.preference_last_name), lastName)
                     putInt(getString(R.string.preference_weight), weight)
                     putInt(getString(R.string.preference_height), height)
                     putInt(getString(R.string.preference_gender), gender)
-                    apply()
+                    commit()
                 }
-
                 finish()
+
             } catch (ex: ClassCastException) {
-                Toast.makeText(baseContext, getString(R.string.validation_error), Toast.LENGTH_LONG).show()
+                Toast.makeText(baseContext, getString(R.string.validation_error), Toast.LENGTH_LONG)
+                    .show()
             }
 
         }
--- app/src/main/java/com/example/runningapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/profile/ProfileFragment.kt
@@ -1,31 +1,163 @@
 package com.example.runningapp.ui.profile
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.runningapp.R
+import com.example.runningapp.ui.history.WorkoutViewModel
+import com.example.runningapp.ui.history.WorkoutViewModelFactory
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.profile_achievements.*
+import java.util.concurrent.TimeUnit
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: ProfileViewModel
+    private var sharedPref: SharedPreferences? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_profile)
-//        notificationsViewModel.text.observe(this, Observer {
-//            textView.text = it
-//        })
-        return root
+        Log.d("Tag", "Inside onCreateView")
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("Tag", "Inside onViewCreated")
+
+
+        val viewModel =
+            WorkoutViewModelFactory(activity!!.application).create(WorkoutViewModel::class.java)
+        viewModel.workoutList.observe(this, Observer { workoutList ->
+            workoutList?.let {
+                achievements_trainings_count.text = workoutList.size.toString()
+                achievements_distance_sum.text =
+                    Math.round(workoutList.sumByDouble { workout -> workout.routeSections.sumByDouble { section -> section.distance.toDouble() } } / 1000)
+                        .toString()
+
+                achievements_time_sum.text =
+                    (workoutList.sumBy { workout -> workout.timeInMillis.toInt() } / 36000000).toString()
+                achievements_calories_sum.text =
+                    workoutList.sumBy { workout -> workout.caloriesBurnt }.toString()
+
+                achievements_best_distance.text =
+                    (Math.round(workoutList.maxBy {
+                            workout -> workout.routeSections.sumByDouble{
+                            section -> section.distance.toDouble() } }!!.routeSections.sumByDouble{
+                            section -> section.distance.toDouble() } * 100.0) / 100).toString()
+                        .plus("m")
+            }
+
+            achievements_best_calories.text =
+                workoutList.maxBy { workout -> workout.caloriesBurnt }!!.caloriesBurnt.toString()
+                    .plus("kcal")
+
+            val longestTime = workoutList.maxBy { workout -> workout.timeInMillis }!!.timeInMillis
+            achievements_longest_time.text = if (longestTime >= 3600000)
+                String.format(
+                    "%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(longestTime),
+                    TimeUnit.MILLISECONDS.toMinutes(longestTime) % TimeUnit.HOURS.toMinutes(1),
+                    TimeUnit.MILLISECONDS.toSeconds(longestTime) % TimeUnit.MINUTES.toSeconds(1)
+                )
+            else
+                String.format(
+                    "%02d:%02d",
+                    TimeUnit.MILLISECONDS.toMinutes(longestTime) % TimeUnit.HOURS.toMinutes(1),
+                    TimeUnit.MILLISECONDS.toSeconds(longestTime) % TimeUnit.MINUTES.toSeconds(1)
+                )
+        })
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.d("Tag", "Inside onActivityCreated")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Tag", "Inside onStart")
+        sharedPref =
+            activity?.getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE)
+
+        val firstname = sharedPref!!.getString(getString(R.string.preference_first_name), "John")
+        val lastname = sharedPref!!.getString(getString(R.string.preference_last_name), "Doe")
+        profile_user_name.text = firstname.plus(" ").plus(lastname)
+        profile_user_height.text =
+            (sharedPref!!.getInt(getString(R.string.preference_height), 70)).toString()
+        profile_user_weight.text =
+            (sharedPref!!.getInt(getString(R.string.preference_weight), 165)).toString()
+
+        when (sharedPref!!.getInt(getString(R.string.preference_gender), 0)) {
+            0 -> {
+                profile_gender.text = getString(R.string.male)
+            }
+            1 -> {
+                profile_gender.text = getString(R.string.female)
+            }
+            else -> return
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Tag", "Inside onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Tag", "Inside onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Tag", "Inside onStop")
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("Tag", "Inside onDestroyView")
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Tag", "Inside onDestroy")
+
+    }
+
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        sharedPref = activity?.getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE)
+//
+//        val firstname = sharedPref!!.getString(getString(R.string.preference_first_name),"John")
+//        val lastname = sharedPref!!.getString(getString(R.string.preference_last_name),"Doe")
+//        profile_user_name.text = firstname.plus(" ").plus(lastname)
+//      //  profile_user_height.text = (sharedPref!!.getInt(getString(R.string.preference_height),70)).toString()
+//        profile_user_weight.text = (sharedPref!!.getInt(getString(R.string.preference_weight),165)).toString()
+//
+//        when(sharedPref!!.getInt(getString(R.string.preference_gender),0)){
+//            0 -> {
+//                profile_gender.text = getString(R.string.male)
+//            }
+//            1 ->{profile_gender.text = getString(R.string.female)}
+//            else -> return
+//        }
+//    }
 }
